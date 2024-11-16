6c38a6eef152533999c80e92f34627bf21ef4765
Mohammad Sulthan <muhammadsultana@outlook.com>
2024-11-16 11:22:49 +0700

    Initial Commit

---
+++ trump-quotes/trump_quotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  trump_quotesApp.swift
+//  trump-quotes
+//
+//  Created by Mohammad Sulthan on 16/11/24.
+//
+
+import SwiftUI
+
+@main
+struct trump_quotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
