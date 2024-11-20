06263d3015701eb90e955081281f8f6ce14c81ec
Mohammad Sulthan <muhammadsultana@outlook.com>
2024-11-20 23:00:23 +0700

    update assets and add notification

---
+++ trump-quotes/Views/Settings.swift
@@ -0,0 +1,85 @@
+//
+//  Settings.swift
+//  trump-quotes
+//
+//  Created by Mohammad Sulthan on 20/11/24.
+//
+
+import Combine
+import SwiftUI
+import UserNotifications
+
+class NotificationManager {
+    static let instance = NotificationManager() // Singleton
+    
+    func requestAuthorization() {
+        let options: UNAuthorizationOptions = [.alert]
+        UNUserNotificationCenter.current().requestAuthorization(options: options) { _, error in
+            if let error = error {
+                print("ERROR: \(error)")
+            } else {
+                self.scheduleDailyNotification()
+            }
+        }
+    }
+    
+    func scheduleDailyNotification() {
+        let content = UNMutableNotificationContent()
+        content.title = "Donald J Trump:"
+        content.subtitle = "MAKE AMERICA GREAT AGAIN!"
+        content.sound = .default
+        
+        var dateComponents = DateComponents()
+        dateComponents.hour = 9
+        dateComponents.minute = 0
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true )
+        
+        let request = UNNotificationRequest(
+            identifier: UUID().uuidString,
+            content: content,
+            trigger: trigger
+        )
+        
+        UNUserNotificationCenter.current().add(request)
+    }
+    
+    func cancelNotification() {
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+        UNUserNotificationCenter.current().removeAllDeliveredNotifications()
+    }
+}
+
+struct Settings: View {
+    @State private var isNotificationOn = false
+    var body: some View {
+        NavigationView {
+            List {
+                Section(
+                    header: Text("General Settings"),
+                    footer: Text("This will allow you get daily quote at 9:00 AM")
+                ) {
+                    Toggle("Turn on notification", isOn: $isNotificationOn)
+                        .toggleStyle(SwitchToggleStyle())
+                        .tint(Color.accentColor)
+                        .onChange(of: isNotificationOn) { _isNotificationOn in
+                            if _isNotificationOn {
+                                NotificationManager.instance.requestAuthorization()
+                            } else {
+                                stopNotification()
+                            }
+                        }
+                }
+            }
+            .navigationTitle("Settings")
+            .listStyle(InsetGroupedListStyle())
+        }
+    }
+    
+    func stopNotification() {
+        NotificationManager.instance.cancelNotification()
+    }
+}
+
+#Preview {
+    Settings()
+}
---
+++ trump-quotes/Views/TodayQuote.swift
@@ -0,0 +1,170 @@
+//
+//  TodayQuote.swift
+//  trump-quotes
+//
+//  Created by Mohammad Sulthan on 20/11/24.
+//
+
+import SwiftUI
+
+struct ImageOverlay: View {
+    var quote: String
+
+    var body: some View {
+        VStack {
+            Text(quote)
+                .font(.title)
+                .padding(.horizontal, 5)
+                .foregroundStyle(.white)
+
+            Spacer()
+                .frame(height: 4)
+
+            Text("Donald J. Trump")
+                .font(.callout)
+                .fontWeight(.medium)
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                .italic()
+                .padding()
+                .foregroundStyle(.white)
+        }
+        .background(Color.accentColor)
+        .opacity(0.8)
+        .cornerRadius(8)
+        .padding(8)
+    }
+}
+
+struct TodayQuote: View {
+    @State private var isLoading: Bool = false
+    @StateObject private var service = SpreadsheetService()
+
+    @State private var todaysQuote: String = ""
+    private let quoteManager = QuoteManager()
+
+    @State private var showingSheet = false
+
+    var body: some View {
+        if #available(iOS 17.0, *) {
+            NavigationStack {
+                VStack {
+                    Image(uiImage: UIImage(named: "trump-shot")!)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxWidth: .infinity)
+                        .clipShape(RoundedRectangle(cornerRadius: 4))
+                        .padding()
+
+                    Spacer()
+                        .frame(height: 4)
+
+                    VStack {
+                        Text(todaysQuote)
+                            .font(.largeTitle)
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .italic()
+                            .foregroundStyle(Color.white)
+
+                        Spacer()
+                            .frame(height: 16)
+
+                        Text("Donald J. Trump")
+                            .font(.footnote)
+                            .fontWeight(.medium)
+                            .frame(maxWidth: .infinity, alignment: .trailing)
+                            .italic()
+                            .padding()
+                            .foregroundStyle(Color.white)
+                    }
+                    .background(Color.accentColor)
+                    .clipShape(RoundedRectangle(cornerRadius: 4))
+                    .padding(.vertical)
+                    .padding(.horizontal)
+                }
+                .frame(maxHeight: .infinity, alignment: .top)
+                .navigationTitle("Today Quote")
+                .navigationBarTitleDisplayMode(.automatic)
+                .toolbar {
+//                    if isLoading {
+//                        ToolbarItem(placement: .topBarLeading) {
+//                            Button(action: {
+//                                print("Loading button tapped!")
+//                            }) {
+//                                Image(systemName: "circle.fill")
+//                                    .foregroundStyle(.green)
+//                            }
+//                        }
+//                    } else {
+//                        ToolbarItem(placement: .topBarLeading) {
+//                            Button(action: {
+//                                isLoading = true
+//                                Task {
+//                                    await service.fetchQuotes()
+//                                }
+//                                isLoading = false
+//                            }) {
+//                                Image(systemName: "arrow.triangle.2.circlepath")
+//                            }
+//                        }
+//                    }
+
+                    ToolbarItem(placement: .topBarTrailing) {
+                        ShareLink(
+                            item: renderQuotedImage(),
+                            preview: SharePreview("Donald J. Trump", image: renderQuotedImage()) // Optional preview
+                        ) {
+                            Label("Share Content", systemImage: "square.and.arrow.up")
+                                .font(.headline)
+                                .padding()
+                                .background(Color.blue)
+                                .foregroundColor(.white)
+                                .cornerRadius(8)
+                        }
+                    }
+                }
+            }
+            .onAppear {
+                loadTodaysQuote()
+            }
+        } else {}
+    }
+
+    private func renderQuotedImage() -> Image {
+        let renderer = ImageRenderer(content: ZStack {
+            Image("trump-shot")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+
+            Color.black.opacity(0.2)
+
+            Spacer()
+        }
+        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+        .cornerRadius(6)
+        .overlay(
+            VStack {
+                Spacer()
+                ImageOverlay(quote: todaysQuote)
+                    .padding(.bottom, 24)
+                    .cornerRadius(16)
+            }
+        )
+        )
+
+        return Image(uiImage: renderer.uiImage!)
+    }
+
+    private func loadTodaysQuote() {
+        todaysQuote = quoteManager.getTodaysQuote()
+    }
+
+    private func refreshQuote() {
+        todaysQuote = quoteManager.getTodaysQuote()
+    }
+}
+
+#Preview {
+    TodayQuote()
+}
